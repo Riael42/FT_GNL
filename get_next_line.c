de0a378a6f27e82c5376495228464688f8dfc6b9
Riael Campus <riael@campus>
2026-06-11 18:42:41 +0200

    a

--- get_next_line.c
+++ get_next_line.c
@@ -3,100 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: riael <#42gether | #voteme4council>        +#+  +:+       +#+        */
+/*   By: riael <gpascu@student.42berlin.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/07 17:04:32 by riael             #+#    #+#             */
-/*   Updated: 2023/07/16 21:42:50 by riael            ###   ########.fr       */
+/*   Created: 2026/06/11 16:28:13 by riael             #+#    #+#             */
+/*   Updated: 2026/06/11 18:13:53 by riael            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
-//#define malloc(x) NULL
 
 char	*get_next_line(int fd)
 {
-	static char	leftovers[1024][BUFFER_SIZE + 1] = {0};
-	ssize_t		i;
-	char		*buf;
-	char		cup[BUFFER_SIZE + 1];
+	static t_gnldat	data;
 
 	if (fd < 0 || fd > 1023)
 		return ((void *)0);
-	i = 1;
-	buf = ft_strjoin(leftovers[fd], ((void *)0), 0);
-	if (ft_found_nl(buf) != 0)
-		return (ft_hook_nl(buf, leftovers[fd]));
-	while (i > 0)
+	ft_init(*data, fd);
+	if	(checknl(&data) != 0)
+		return (good_ret(&data));
+	while (data.rv > 0)
 	{
-		i = ft_fill_cup(cup, fd);
-		if (i < 0)
-			return (ft_exit_gracefully(buf, leftovers[fd]));
-		buf = ft_strjoin(buf, cup, 1);
-		if (buf == ((void *)0))
-			return ((void *)0);
-		if (ft_found_nl(buf) != 0)
-			return (ft_hook_nl(buf, leftovers[fd]));
+		readfunc(&data);
+		if (data.err != 0)
+			return (ft_exit_gracefully(&data));
+		data.ret = ft_strjoin(data.ret, data.buf, 1);
+		if (data.err != 0)
+			return (ft_exit_gracefully(&data));
+		if (checknl(&data) != 0)
+			return (good_ret(&data));
 	}
-	if (ft_found_nl(buf) != 0)
-		return (ft_hook_nl(buf, leftovers[fd]));
+		if (check(buf) != 0)
+		return good
 	return (ft_last_line(buf, leftovers[fd]));
 }
 
-ssize_t	ft_fill_cup(char *cup, int fd)
+ft init
 {
-	ssize_t	i;
+	data.fd = fd;
+	data.found = 0;
+	data.err = 0;
+	data.rv = 1;
+	data->leftovers[fd] = malloc;
+	data.buf = //join leftovers with nothing
 
-	i = read(fd, cup, BUFFER_SIZE);
-	if (i < 0)
-		cup[0] = '\0';
-	else
-		cup[i] = '\0';
-	return (i);
-}
-
-char	*ft_exit_gracefully(char *buf, char *leftovers)
-{
-	int	i;
-
-	i = 0;
-	if (buf != ((void *)0))
-	{
-		free(buf);
-		buf = ((void *)0);
-	}
-	while (leftovers[i] != '\0')
-	{
-		leftovers[i] = '\0';
-		i += 1;
-	}
-	return (buf);
-}
-
-ssize_t	ft_strlen(char *s)
-{
-	ssize_t	i;
-
-	if (s == ((void *)0))
-		return (0);
-	i = 0;
-	while (s[i] != '\0')
-		i += 1;
-	return (i);
-}
-
-char	*ft_last_line(char *buf, char *leftovers)
-{
-	if (buf == ((void *)0))
-		return ((void *)0);
-	if (leftovers[0] == '\0')
-	{
-		if (buf[0] != '\0')
-		{
-			leftovers[0] = '\0';
-			return (buf);
-		}
-		free(buf);
-		return ((void *)0);
-	}
-	leftovers[0] = '\0';
-	return (buf);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,94 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: riael <#42gether | home@42berlin>          +#+  +:+       +#+        */
+/*   By: riael <gpascu@student.42berlin.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/21 19:17:45 by riael             #+#    #+#             */
-/*   Updated: 2024/01/17 11:33:05 by riael            ###   ########.fr       */
+/*   Created: 2026/06/11 16:35:26 by riael             #+#    #+#             */
+/*   Updated: 2026/06/11 18:01:54 by riael            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
-char	*ft_hook_nl(char *buf, char *leftovers)
+void	readfunc(t_gnldat *data)
 {
-	ssize_t	i;
-	ssize_t	j;
-
-	i = 0;
-	j = 0;
-	while (buf[i] != '\n')
-		i += 1;
-	i += 1;
-	if (buf[i] == '\0')
+	data->rv = read(data.fd, data.buf, BUFFER_SIZE);
+	if (data->rv == -1)
 	{
-		leftovers[j] = '\0';
-		return (buf);
-	}
-	while (buf[i] != '\0')
-	{
-		leftovers[j] = buf[i];
-		buf[i] = '\0';
-		i += 1;
-		j += 1;
-	}
-	leftovers[j] = '\0';
-	return (buf);
-}
-
-void	ft_strcat(char *s1, char *s2)
-{
-	ssize_t	i;
-	ssize_t	j;
-
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
-		i += 1;
-	while (s2[j] != '\0')
-	{
-		s1[i] = s2[j];
-		i += 1;
-		j += 1;
-	}
-	s1[i] = '\0';
-}
-
-int	ft_found_nl(char *buf)
-{
-	ssize_t	i;
-
-	if (buf == ((void *)0))
-		return (0);
-	i = 0;
-	while (buf[i] != '\0' && buf[i] != '\n')
-		i += 1;
-	if (buf[i] == '\n')
-		return (1);
-	return (0);
-}
-
-char	*ft_strjoin(char *s1, char *s2, char x)
-{
-	ssize_t	i;
-	char	*ret;
-
-	ret = (char *)malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (ret == ((void *)0))
-		return ((void *)0);
-	ret[0] = '\0';
-	if (s1 != ((void *)0))
-	{
-		i = 0;
-		while (s1[i] != '\0')
+		int clean = 0;
+		while (clean < 1024)
 		{
-			ret[i] = s1[i];
-			i += 1;
+			if (data->leftovers[clean] != ((void *)0))
+				free(data.leftovers[clean]);
+			clean += 1;
 		}
-		ret[i] = '\0';
-		if (x == 1)
-			free(s1);
+		data->err = 1;
 	}
-	if (s2 != ((void *)0))
-		ft_strcat(ret, s2);
-	return (ret);
+}
+
+int	checknl(t_gnldat *data)
+{
+	int	j;
+
+	j = 0;
+	while (data->buf[j] != '\0')
+	{
+		if (data->buf[j] == '\n')
+			data->found = j;
+		j += 1;
+	}
+	data.bufsize = j * (data.found == 0) + data.found * (data.found != 0) + 1;
+	return (found);
+}
+
+char	*good_ret(t_gnldat *data)
+{
+	int	k;
+
+	k = 0;
+	if (data->leftovers[fd] != ((void *)0))
+	{
+		while (data->buf[data->found] != '\0')
+		{
+			data->leftovers[fd][k] = data->buf[found];
+			k += 1;
+			found += 1;
+		}
+		data->leftovers[fd][k] = '\0';
+	}
+	return (data->ret);
+}
+
+void	ft_strjoin(t_gnldat *data)
+{
+	char	*newret;
+
+	data->retsize += data->bufsize;
+	newret = (char *)malloc(sizeof(char) * data->retsize);
+	if (newret == ((void *)0))
+		data->err = 1;
+	if (data->err != 1)
+	{
+		ft_strncpy(newret, data->ret, data->retsize - data->bufsize - 1);
+		ft_strcat(newret, data->buf);
+	}
+	free(data->ret);
+	data->ret = newret;
 }
